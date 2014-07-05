8d871506d3e688f37e11d17632fb4cec94022c1d
Thomas Amland <thomas.amland@gmail.com>
2014-07-05 15:05:51 +0200

    add some more tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -17,21 +17,45 @@
 
 from __future__ import unicode_literals
 import logging
+import pytest
 import wimpy
 
 logging.basicConfig(level=logging.DEBUG)
 
-def test_artist():
-    session = wimpy.Session()
-    albums = session.get_albums(18888)
-    assert len(albums) > 0
+@pytest.fixture()
+def session():
+    return wimpy.Session()
 
-def test_album():
-    session = wimpy.Session()
-    tracks = session.get_album(16909093)
-    assert len(tracks) > 0
 
-def test_search():
-    session = wimpy.Session(session_id='asd')
+def test_artist(session):
+    artist_id = 18888
+    artist = session.get_artist(artist_id)
+    assert artist.id == artist_id
+    assert artist.name == 'Mala'
+
+
+def test_album(session):
+    album_id = 16909093
+    album = session.get_album(album_id)
+    assert album.id == album_id
+    assert album.name == 'Mala in Cuba'
+    assert album.num_tracks == 14
+    assert album.duration == 3437
+    assert album.artist.name == 'Mala'
+
+
+def test_get_album_tracks(session):
+    tracks = session.get_album_tracks(16909093)
+    assert tracks[0].name == 'Introduction'
+    assert tracks[0].track_num == 1
+    assert tracks[0].duration == 155
+    assert tracks[-1].name == 'Noche Sueños'
+    assert tracks[-1].track_num == 14
+    assert tracks[-1].duration == 338
+    assert tracks[0].artist.name == 'Mala'
+    assert tracks[0].album.name == 'Mala in Cuba'
+
+
+def test_search(session):
     artists = session.search('artists', 'mala')
     assert artists[0].name == "Mala"

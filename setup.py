e4955028346eca8725043a710e2d2fcb79c699b1
Hugo Perrin <hugo1344.perrin@gmail.com>
2022-05-24 22:05:51 +0200

    chore: bump version to 0.6.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('HISTORY.rst') as f:
 
 setup(
     name='tidalapi',
-    version='0.6.6',
+    version='0.6.7',
     description='Unofficial API for TIDAL music streaming service.',
     long_description=long_description,
     author='Thomas Amland',

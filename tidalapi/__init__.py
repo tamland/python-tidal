a6a5a6410c5fbc08bc9370838fdd95c30551da21
John Maximilian <2e0byo@gmail.com>
2023-07-08 22:12:33 +0100

    chore: noqa on re-exports.

--- tidalapi/__init__.py
+++ tidalapi/__init__.py
@@ -1,12 +1,18 @@
-from .album import Album
-from .artist import Artist, Role
-from .genre import Genre
-from .media import Track, Video
-from .mix import Mix
-from .page import Page
-from .playlist import Playlist, UserPlaylist
-from .request import Requests
-from .session import Config, Quality, Session, VideoQuality
-from .user import Favorites, FetchedUser, LoggedInUser, PlaylistCreator, User
+from .album import Album  # noqa: F401
+from .artist import Artist, Role  # noqa: F401
+from .genre import Genre  # noqa: F401
+from .media import Track, Video  # noqa: F401
+from .mix import Mix  # noqa: F401
+from .page import Page  # noqa: F401
+from .playlist import Playlist, UserPlaylist  # noqa: F401
+from .request import Requests  # noqa: F401
+from .session import Config, Quality, Session, VideoQuality  # noqa: F401
+from .user import (  # noqa: F401
+    Favorites,
+    FetchedUser,
+    LoggedInUser,
+    PlaylistCreator,
+    User,
+)
 
 __version__ = "0.7.1"

94a74063107fa0b6e1c5985dd44545adea9976ce
tehkillerbee <tehkillerbee@users.noreply.github.com>
2024-02-06 22:10:18 +0100

    Added custom exceptions

---
+++ tidalapi/exceptions.py
@@ -0,0 +1,18 @@
+class LoginError(Exception):
+    pass
+
+
+class MediaUnknown(Exception):
+    pass
+
+
+class UnknownManifestFormat(Exception):
+    pass
+
+
+class MediaMissing(Exception):
+    pass
+
+
+class StreamManifestDecodeError(Exception):
+    pass
